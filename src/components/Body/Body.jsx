380da9004f29160c3060120ab231a5a5e4b39620
a-ssh1ey <annasoboleva0507@gmail.com>
2024-08-24 12:23:19 +0200

    24.08_idk_buttons

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,15 +1,29 @@
-import Button from "../Button/Button";
+import { Button } from "../styled/styled";
 import React from "react";
 import styles from "./Body.module.css";
 
-function Body({setActive, selected}) {
+function Body({ setActive, selected }) {
   return (
     <div className={styles.body}>
-      <Button text="Join a deal" inactive onClick={() => setActive(1)} disabled={selected === 1}/>
-      <Button text="Create a deal" inactive onClick={() => setActive(2)} disabled={selected === 2}/>
-      <Button text="Pending deals" inactive onClick={() => setActive(3)} disabled={selected === 3}/>
+      <Button
+        text="Join a deal"
+        inactive
+        onClick={() => setActive(1)}
+        disabled={selected === 1}
+      />
+      <Button
+        text="Create a deal"
+        inactive
+        onClick={() => setActive(2)}
+        disabled={selected === 2}
+      />
+      <Button
+        text="Pending deals"
+        inactive
+        onClick={() => setActive(3)}
+        disabled={selected === 3}
+      />
     </div>
-
   );
 }
 
