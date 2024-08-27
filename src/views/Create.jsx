e6c3e685c80fad97a0a1e7e24fe16e08d8d48995
a-ssh1ey <annasoboleva0507@gmail.com>
2024-08-27 14:37:53 +0200

    27.08_create

--- src/views/Create.jsx
+++ src/views/Create.jsx
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
 import Button_extra from "../components/Button_extra/Button_extra";
 import axios from "axios";
-//import { URL } from "../config.js";
+import { APIURL } from "../../configure";
 
 function Create({ setActive, selected }) {
   const [text, setText] = useState("");
 
   const handleCreateDeal = () => {
-    /*console.log('Sending request...');
-        axios.post(`${URL}/playground/create-deal/`)
-            .then(response => {
-                console.log('Response received:', response.data);
-                setText(`Deal created with code: ${response.data.code}`);
-            })
-            .catch(error => {
-                console.error('Error during request:', error);
-                setText('Error creating deal.');
-            });*/
+    console.log("Sending request...");
+    axios
+      .post(`${APIURL}/playground/create-deal/`)
+      .then((response) => {
+        console.log("Response received:", response.data);
+        setText(`Deal created with code: ${response.data.code}`);
+      })
+      .catch((error) => {
+        console.error("Error during request:", error);
+        setText("Error creating deal.");
+      });
   };
 
   return (
@@ -31,6 +32,7 @@ function Create({ setActive, selected }) {
       <Button_extra
         text="Create Deal"
         onClick={handleCreateDeal}
+        s
         secondary={false}
       />
       <p>{text}</p>
