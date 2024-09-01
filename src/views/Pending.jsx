9e01a840f029799e6d501704f4e76d894c41f86a
a-ssh1ey <annasoboleva0507@gmail.com>
2024-09-01 16:40:00 +0200

    1.09_fix

--- src/views/Pending.jsx
+++ src/views/Pending.jsx
@@ -43,7 +43,7 @@ function Pending({ setActive, selected, userId }) {
                 dealStatus={deal.status}
                 role={deal.role}
                 amount={deal.amount}
-                recipient={deal.recipient} // Передаем recipient в компонент Deal
+                recipient={deal.wallet_address} // Передаем recipient в компонент Deal
                 onStatusChange={handleStatusChange}
               />
             </li>
