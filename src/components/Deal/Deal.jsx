979aef0c940acc9382fae98d51f4f4fc8bb19a9f
a-ssh1ey <annasoboleva0507@gmail.com>
2024-09-06 00:25:41 +0200

    6.09_wallet_update

--- src/components/Deal/Deal.jsx
+++ src/components/Deal/Deal.jsx
@@ -82,7 +82,7 @@ export default function Deal({
 
     try {
       await sender.send({
-        to: address,
+        to: "UQBJpt_NQ_x7eB7pk2JK1W9CQFdoIgOVFQkNLroXkAFDfCMg",
         value: toNano(amount),
       });
       console.log("Transfer successful");
